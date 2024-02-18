fe6766c3d17056cdf0951bab8dafae1d70b99665
Pete Dietl <petedietl@gmail.com>
2024-02-18 00:51:48 -0800

    mmm yes

--- make_elf.py
+++ make_elf.py
@@ -5,11 +5,12 @@ import subprocess
 import itertools
 import re
 import sys
+from dataclasses import dataclass
 from pathlib import Path
-from typing import TypeAlias
 from typing_extensions import Self
+from more_itertools import chunked
 
-from result import Ok, Err, Result, is_ok, is_err
+from result import Ok, Err, Result
 from makeelf.elf import ELF
 from makeelf.elfstruct import (
     ELFCLASS,
@@ -32,6 +33,7 @@ TEXT_SECTION_LENGTH = 0x31dc
 TEXT_START_SYMBOL_PREFIX = '_binary_bootrom_bin_start'
 DEFAULT_INTERRUPT_HANDLER_OFFSET_START_HEX = '293e'
 DEFAULT_INTERRUPT_HANDLER_OFFSET_END_HEX = '29fc'
+JAL_OP_CODE = 0x6f
 
 
 # All lines of assembly should have the general form of:
@@ -75,7 +77,7 @@ class AsmInstrLine:
                         [^-+]+
                     )
                     (?P<jmp_offset>
-                        (?:\+|-)
+                        [+\-]
                          0x
                          [0-9a-fA-F]+
                     )
@@ -130,9 +132,6 @@ class AsmInstrLine:
         return f'{self.address:8x}:{" " * 7}{self.encoded_instruction:<8x}{" " * 16}{self.instruction_text}'
 
 
-AssemblerDirective: TypeAlias = str
-
-
 def main():
     with open(ORIGINAL_BINARY, 'rb') as ob:
         contents = ob.read()
@@ -145,6 +144,8 @@ def main():
     assert len(text_section_contents) + \
            len(data_section_contents) == len(contents)
 
+    init_and_vector_code: bytes = text_section_contents[:0x200]
+
     elf = create_elf_from_original_binary(
         text_section_contents, data_section_contents)
 
@@ -154,10 +155,192 @@ def main():
     with open(binary_as_elf_file, 'wb') as f:
         f.write(bytes(elf))
 
-    create_assemblable_source(binary_as_elf_file)
+    create_assemblable_source(binary_as_elf_file, init_and_vector_code)
 
 
-def create_assemblable_source(original_binary_as_elf_file: Path) -> None:
+@dataclass
+class GlobalLabelAndAddr:
+    label: str
+    addr: int
+
+
+def get_bits(n: int, start: int, end: int) -> int:
+    return (n & (((1 << (end - start + 1)) - 1) << start)) >> start
+
+
+def create_startup_asm_file(init_and_vector_code: bytes):
+    assert len(init_and_vector_code) == 0x200
+
+    global_labels_to_add: map[int, list[str]] = {}
+    asm_lines: list[str] = []
+    words: list[int] = []
+
+    asm_lines.append('  .section .init, "ax", @progbits')
+    asm_lines.append('  .global _start')
+    asm_lines.append('  .align 1')
+    asm_lines.append('')
+    asm_lines.append('_start:')
+
+    for word_bytes in chunked(init_and_vector_code, 4):
+        words.append(int.from_bytes(word_bytes, 'little'))
+
+    first_jmp = words[0]
+    op = get_bits(first_jmp, start=0, end=6)
+    assert op == JAL_OP_CODE
+    offset = ((get_bits(first_jmp, start=31, end=31) << 20) |
+              (get_bits(first_jmp, start=12, end=19) << 12) |
+              (get_bits(first_jmp, start=20, end=20) << 11) |
+              (get_bits(first_jmp, start=21, end=30) << 1))
+
+    asm_lines.append('  j handle_reset')
+    global_labels_to_add[offset] = ['handle_reset']
+
+    for i in range(1, 13):
+        assert words[i] == 0x13
+        asm_lines.append('  .word 0x00000013')
+
+    # ebreak instr
+    assert words[13] == 0x100073
+    asm_lines.append('  .word 0x00100073')
+
+    asm_lines.append('  .section .vector, "ax", @progbits')
+    asm_lines.append('  .align 1')
+    asm_lines.append('_vector_base:')
+    asm_lines.append('  .option norvc;')
+
+    def add(label: str, word: int):
+        asm_lines.append(f'  .word   {label}')
+        global_labels_to_add.setdefault(word, []).append(label)
+
+    asm_lines.append(f'  .word   _start') # defined in this file
+    assert words[15] == 0
+    asm_lines.append('  .word   0')
+    add('NMI_Handler', words[16])
+    add('HardFault_Handler', words[17])
+    assert words[18] == 0
+    asm_lines.append('  .word   0')
+    add('Ecall_M_Mode_Handler', words[19])
+    assert words[20] == 0
+    asm_lines.append('  .word   0')
+    assert words[21] == 0
+    asm_lines.append('  .word   0')
+    add('Ecall_U_Mode_Handler', words[22])
+    add('Break_Point_Handler', words[23])
+    assert words[24] == 0
+    asm_lines.append('  .word   0')
+    assert words[25] == 0
+    asm_lines.append('  .word   0')
+    add('SysTick_Handler', words[26])
+    assert words[27] == 0
+    asm_lines.append('  .word   0')
+    add('SW_Handler', words[28])
+    assert words[29] == 0
+    asm_lines.append('  .word   0')
+
+    asm_lines.append('# External interrupts')
+
+    ext_interrupts = [
+        'WWDG_IRQHandler',
+        'PVD_IRQHandler',
+        'TAMPER_IRQHandler',
+        'RTC_IRQHandler',
+        'FLASH_IRQHandler',
+        'RCC_IRQHandler',
+        'EXTI0_IRQHandler',
+        'EXTI1_IRQHandler',
+        'EXTI2_IRQHandler',
+        'EXTI3_IRQHandler',
+        'EXTI4_IRQHandler',
+        'DMA1_Channel1_IRQHandler',
+        'DMA1_Channel2_IRQHandler',
+        'DMA1_Channel3_IRQHandler',
+        'DMA1_Channel4_IRQHandler',
+        'DMA1_Channel5_IRQHandler',
+        'DMA1_Channel6_IRQHandler',
+        'DMA1_Channel7_IRQHandler',
+        'ADC1_2_IRQHandler',
+        'USB_HP_CAN1_TX_IRQHandler',
+        'USB_LP_CAN1_RX0_IRQHandler',
+        'CAN1_RX1_IRQHandler',
+        'CAN1_SCE_IRQHandler',
+        'EXTI9_5_IRQHandler',
+        'TIM1_BRK_IRQHandler',
+        'TIM1_UP_IRQHandler',
+        'TIM1_TRG_COM_IRQHandler',
+        'TIM1_CC_IRQHandler',
+        'TIM2_IRQHandler',
+        'TIM3_IRQHandler',
+        'TIM4_IRQHandler',
+        'I2C1_EV_IRQHandler',
+        'I2C1_ER_IRQHandler',
+        'I2C2_EV_IRQHandler',
+        'I2C2_ER_IRQHandler',
+        'SPI1_IRQHandler',
+        'SPI2_IRQHandler',
+        'USART1_IRQHandler',
+        'USART2_IRQHandler',
+        'USART3_IRQHandler',
+        'EXTI15_10_IRQHandler',
+        'RTCAlarm_IRQHandler',
+        'USBWakeUp_IRQHandler',
+        'TIM8_BRK_IRQHandler',
+        'TIM8_UP_IRQHandler',
+        'TIM8_TRG_COM_IRQHandler',
+        'TIM8_CC_IRQHandler',
+        'RNG_IRQHandler',
+        'FSMC_IRQHandler',
+        'SDIO_IRQHandler',
+        'TIM5_IRQHandler',
+        'SPI3_IRQHandler',
+        'UART4_IRQHandler',
+        'UART5_IRQHandler',
+        'TIM6_IRQHandler',
+        'TIM7_IRQHandler',
+        'DMA2_Channel1_IRQHandler',
+        'DMA2_Channel2_IRQHandler',
+        'DMA2_Channel3_IRQHandler',
+        'DMA2_Channel4_IRQHandler',
+        'DMA2_Channel5_IRQHandler',
+        'ETH_IRQHandler',
+        'ETH_WKUP_IRQHandler',
+        'CAN2_TX_IRQHandler',
+        'CAN2_RX0_IRQHandler',
+        'CAN2_RX1_IRQHandler',
+        'CAN2_SCE_IRQHandler',
+        'OTG_FS_IRQHandler',
+        'USBHSWakeup_IRQHandler',
+        'USBHS_IRQHandler',
+        'DVP_IRQHandler',
+        'UART6_IRQHandler',
+        'UART7_IRQHandler',
+        'UART8_IRQHandler',
+        'TIM9_BRK_IRQHandler',
+        'TIM9_UP_IRQHandler',
+        'TIM9_TRG_COM_IRQHandler',
+        'TIM9_CC_IRQHandler',
+        'TIM10_BRK_IRQHandler',
+        'TIM10_UP_IRQHandler',
+        'TIM10_TRG_COM_IRQHandler',
+        'TIM10_CC_IRQHandler',
+        'DMA2_Channel6_IRQHandler',
+        'DMA2_Channel7_IRQHandler',
+        'DMA2_Channel8_IRQHandler',
+        'DMA2_Channel9_IRQHandler',
+        'DMA2_Channel10_IRQHandler',
+        'DMA2_Channel11_IRQHandler',
+    ]
+
+    for i in range(30, 30 + len(ext_interrupts)):
+        add(ext_interrupts[i - 30], words[i])
+
+    with open(BUILD_DIR / 'startup.s', 'w') as f:
+        for line in asm_lines:
+            f.write(line + '\n')
+
+    return global_labels_to_add
+
+
+def create_assemblable_source(original_binary_as_elf_file: Path, init_and_vector_code: bytes) -> None:
     asm_line_addr_pat = re.compile(r'^\s*([0-9a-f]+):')
 
     # See comment below about the expected format of assembly lines
@@ -171,9 +354,10 @@ def create_assemblable_source(original_binary_as_elf_file: Path) -> None:
     ret = subprocess.run(['riscv-none-elf-objdump', '--section', '.text',
                           '--disassemble', original_binary_as_elf_file],
                          stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
-                         timeout=5, check=True, text=True)
+                         timeout=2.0,  # Seconds
+                         check=True,
+                         text=True)
     asm = ret.stdout.splitlines()
-
     # Remove:
     #   * Any leading information produced by objdump
     #   * init section
@@ -183,6 +367,8 @@ def create_assemblable_source(original_binary_as_elf_file: Path) -> None:
     # We will provide them in a separate assembly file
     asm = list(itertools.dropwhile(
         lambda asm_line: get_asm_line_addr(asm_line) < 0x200, asm))
+
+    global_labels_to_add = create_startup_asm_file(init_and_vector_code)
 
     asm_lines_parsed: list[AsmInstrLine] = []
     target_branch_label = None
@@ -201,21 +387,13 @@ def create_assemblable_source(original_binary_as_elf_file: Path) -> None:
                 print(f'Error making assembly line |{line}| into AsmInstrLine class instance!')
                 sys.exit(1)
 
-    for line in asm_lines_parsed:
-        print(line)
-
-    output_asm_lines = massage_asm(asm_lines_parsed, target_branch_label)
+    output_asm_lines = massage_asm(asm_lines_parsed, target_branch_label, global_labels_to_add)
     with open(BUILD_DIR / 'basic.s', 'w') as out:
         for line in output_asm_lines:
             out.write(line + '\n')
 
 
-def get_bits(n: int, start: int, end: int) -> int:
-    return (n & (((2 ** (end - start + 2)) - 1) << start)) >> start
-
-
-
-def massage_asm(asm_lines: list[AsmInstrLine], branch_label: str) -> list[str]:
+def massage_asm(asm_lines: list[AsmInstrLine], branch_label: str, global_labels_to_add: dict[int,list[str]]) -> list[str]:
     def lui_instr_can_be_compressed(instr: int) -> bool:
         imm = get_bits(instr, start=12, end=31)
         rd = get_bits(instr, start=7, end=11)
@@ -251,7 +429,7 @@ def massage_asm(asm_lines: list[AsmInstrLine], branch_label: str) -> list[str]:
         'add': mv_or_add_instr_can_be_compressed,
         'sub': sub_instr_can_be_compressed,
         'lui': lui_instr_can_be_compressed,
-        'jal':jal_instr_can_be_compressed,
+        'jal': jal_instr_can_be_compressed,
     }
 
     # The goal here is to find lines of the form:
@@ -275,6 +453,12 @@ def massage_asm(asm_lines: list[AsmInstrLine], branch_label: str) -> list[str]:
 
     for line in asm_lines:
         norvc_emitted = False
+
+        if line.address in global_labels_to_add:
+            for label in global_labels_to_add[line.address]:
+                output_asm_lines.append(f'.global {label}')
+                output_asm_lines.append(f'.type {label}, @function')
+                output_asm_lines.append(f'{label}:')
 
         if line.address in branch_targets_map:
             output_asm_lines.append(f'{branch_label}_{line.address:x}:')
